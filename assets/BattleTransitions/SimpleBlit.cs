7689d0c8b4c86cfc21de8b3883116349b33b48c4
ofearos <ophir.benjamin@post.idc.ac.il>
2023-05-21 23:53:29 +0300

    added battle transition script and shader

---
+++ assets/BattleTransitions/SimpleBlit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BattleTransitions
+{
+    [ExecuteInEditMode]
+    public class SimpleBlit : MonoBehaviour
+    {
+        public Material transitionMaterial;
+
+        void OnRenderImage(RenderTexture src, RenderTexture dst)
+        {
+            if (transitionMaterial != null)
+                Graphics.Blit(src, dst, transitionMaterial);
+        }
+
+        public void StartTransition(float duration, bool isBoomerang)
+        {
+            
+        }
+    }
+}
